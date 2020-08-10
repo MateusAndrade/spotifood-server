e530b5ac9398866e91522372e105f15fa77828dc
MateusAndrade <mateus.andrade47@outlook.com>
2020-08-09 23:07:27 -0300

    feat: adding cors implementation

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const routes = require("./src/routes");
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
